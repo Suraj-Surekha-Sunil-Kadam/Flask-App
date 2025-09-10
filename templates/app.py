e5353c50c423d52686b54feba6b1219f2a04992d
Suraj-Surekha-Sunil-Kadam <kadamsuraj086@gmail.com>
2025-09-10 08:10:54 +0530

    Add myvenv/ to .gitignore

---
+++ templates/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+def get_cleaned_data(form_data):
+    gestation = float(form_data['gestation'])
+    parity = int(form_data['parity'])
+    age = float(form_data['age'])
+    height = float(form_data['height'])
+    weight = float(form_data['weight'])
+    smoke = float(form_data['smoke'])
+
+    cleaned_data = {
+        'gestation' : [gestation],
+        "parity": [parity],
+        "age": [age],
+        "height": [height],
+        "weight": [weight],
+        "smoke": [smoke]
+    }
+
+    return cleaned_data
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+# define an endpoint
+@app.route('/predict', methods = ['POST'])
+def get_prediction():
+    #get data from user
+    baby_data_form = request.form
+     
+    baby_data_cleaned = get_cleaned_data(baby_data_form)
+
+    # baby_df = pd.DataFrame(baby_data)
+    # Convert incoming dict of lists to a DataFrame of shape (1, n_features)
+    baby_df = pd.DataFrame({k: v[0] for k, v in baby_data_cleaned.items()}, index=[0])
+
+    # load machine learning trained model
+    with open("D:\\Bulding ML Front end\\model\\model.pkl", "rb") as obj:
+        model = pickle.load(obj)
+
+    # makeing predictions on user data
+
+    prediction = model.predict(baby_df)
+    prediction = round(float(prediction[0]), 2)
+
+    # return response in a json format
+
+    response = {"Prediction" : prediction}
+
+    return render_template('index.html', prediction = prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
